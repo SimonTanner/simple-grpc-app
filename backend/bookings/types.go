28e64fcc87b494f3c6b5e26c89624d5b45a85c57
SimonTanner <simon.tanner@river-island.com>
2020-07-03 00:04:01 +0100

    Adds property struct

--- backend/bookings/types.go
+++ backend/bookings/types.go
@@ -1,9 +1,12 @@
 package bookings
 
+import "time"
+
 type Property struct {
-	Id         int32  `db:"id"`
-	DoorNumber string `db:"door_number"`
-	Address    string `db:"address"`
-	City       string `db:"city"`
-	Country    string `db:"country"`
+	Id         int32     `db:"id"`
+	DoorNumber string    `db:"door_number"`
+	Address    string    `db:"address"`
+	City       string    `db:"city"`
+	Country    string    `db:"country"`
+	CreatedAt  time.Time `db:"created_at"`
 }
