8eb2132648b575f4f2a98d513c08a1f7dda66669
SimonTanner <simon.tanner@river-island.com>
2020-07-03 00:02:21 +0100

    Adds migrations

---
+++ db/migrations/00001_init.down.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+DROP TABLE IF EXISTS bookings.duration;
+
+DROP TABLE IF EXISTS bookings.properties;
+
+DROP TABLE IF EXISTS bookings.users;
+
+COMMIT;
---
+++ db/migrations/00001_init.up.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+-- DROP TABLE schema_migrations;
+
+CREATE SCHEMA IF NOT EXISTS bookings;
+
+CREATE TABLE IF NOT EXISTS bookings.users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS bookings.properties (
+    id SERIAL PRIMARY KEY,
+    door_number VARCHAR(50),
+    address VARCHAR(1000),
+    city VARCHAR(100),
+    country VARCHAR(255),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS bookings.duration (
+    property_id INTEGER NOT NULL REFERENCES bookings.properties(id),
+    user_id INTEGER NOT NULL REFERENCES bookings.users(id),
+    start_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    end_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+COMMIT;
