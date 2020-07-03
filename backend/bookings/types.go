ca3e3259964ae1e1b1a48f9c6b18929e4e174930
SimonTanner <simon.tanner@river-island.com>
2020-07-03 13:15:18 +0100

    Adds conversion methods for db to message

--- backend/bookings/types.go
+++ backend/bookings/types.go
@@ -1,6 +1,11 @@
 package bookings
 
-import "time"
+import (
+	"time"
+
+	pb "github.com/SimonTanner/simple-grpc-app/bookings"
+	"github.com/golang/protobuf/ptypes"
+)
 
 type Property struct {
 	Id         int32     `db:"id"`
@@ -24,4 +29,65 @@ type Booking struct {
 	StartDate  time.Time `db:"start_date"`
 	EndDate    time.Time `db:"end_date"`
 	CreatedAt  time.Time `db:"created_at"`
+}
+
+func (user User) ConvertUserToMsg() (*pb.User, error) {
+	createdAt, err := ptypes.TimestampProto(user.CreatedAt)
+	if err != nil {
+		return &pb.User{}, err
+	}
+
+	userMsg := pb.User{
+		Id:        user.Id,
+		FirstName: user.FirstName,
+		Surname:   user.Surname,
+		CreatedAt: createdAt,
+	}
+
+	return &userMsg, nil
+}
+
+func (prop Property) ConverPropertyToMsg() (*pb.Property, error) {
+	createdAt, err := ptypes.TimestampProto(prop.CreatedAt)
+	if err != nil {
+		return &pb.Property{}, err
+	}
+
+	propertyMsg := pb.Property{
+		Id:         prop.Id,
+		DoorNumber: prop.DoorNumber,
+		Address:    prop.Address,
+		City:       prop.City,
+		Country:    prop.Country,
+		CreatedAt:  createdAt,
+	}
+
+	return &propertyMsg, nil
+}
+
+func (b Booking) ConverPropertyToMsg() (*pb.Booking, error) {
+	startDate, err := ptypes.TimestampProto(b.StartDate)
+	if err != nil {
+		return &pb.Booking{}, err
+	}
+
+	endDate, err := ptypes.TimestampProto(b.EndDate)
+	if err != nil {
+		return &pb.Booking{}, err
+	}
+
+	createdAt, err := ptypes.TimestampProto(b.CreatedAt)
+	if err != nil {
+		return &pb.Booking{}, err
+	}
+
+	bookingMsg := pb.Booking{
+		PropertyId: b.PropertyId,
+		UserId:     b.UserId,
+		StartDate:  startDate,
+		EndDate:    endDate,
+		CreatedAt:  createdAt,
+	}
+
+	return &bookingMsg, nil
 }
