02b529ffed2cfeaa520d888eeec1875fb5776f06
SimonTanner <simon.tanner@river-island.com>
2020-07-03 12:06:28 +0100

    Adds user & booking types for db

--- backend/bookings/types.go
+++ backend/bookings/types.go
@@ -10,3 +10,18 @@ type Property struct {
 	Country    string    `db:"country"`
 	CreatedAt  time.Time `db:"created_at"`
 }
+
+type User struct {
+	Id        int32     `db:"id"`
+	FirstName string    `db:"first_name"`
+	Surname   string    `db:"surname"`
+	CreatedAt time.Time `db:"created_at"`
+}
+
+type Booking struct {
+	PropertyId int32     `db:"property_id"`
+	UserId     int32     `db:"user_id"`
+	StartDate  time.Time `db:"start_date"`
+	EndDate    time.Time `db:"end_date"`
+	CreatedAt  time.Time `db:"created_at"`
+}
